9f33a2745b13c15590048c91dd33cd3ce8603f50
Fahd Mannaa <fahdmannaa@macs-Mac-mini.home>
2020-10-09 19:44:52 +0300

    12 Review + full 13 -- this is initial commit

--- accounts/forms.py
+++ accounts/forms.py
@@ -19,4 +19,4 @@ class UserForm(forms.ModelForm):
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['phone_number' , 'addres']
+        fields = ['phone_number' , 'addres', 'image']
